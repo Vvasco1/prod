ca4afc6eddccaf53a47dc420e95ba45508b4e9c8
Vvasco1 <109784466+Vvasco1@users.noreply.github.com>
2023-10-23 16:05:59 +0900

    Update and rename main.tf to output.tf

---
+++ network/output.tf
@@ -0,0 +1,12 @@
+#bastion ec2 생성에 필요한 3가지만 우선적으로 output
+output "vpc_id" {
+	value = aws_vpc.prod.id
+}
+
+output "nat_gw" {
+	value = aws_nat_gateway.natgw
+}
+
+output "subnet_public_a" {
+	value = aws_subnet.pub_a.id
+}
